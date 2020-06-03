15610db879883eb6f3c92081e9b02a8e942dca9e
Alexmg86 <alexmg86@mail.ru>
2020-06-03 18:45:49 +0300

    Удаление группы

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -23,16 +23,6 @@ class GroupController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -62,17 +52,6 @@ class GroupController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -93,6 +72,7 @@ class GroupController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Group::where('id', $id)->delete();
+        return Auth::user()->groups()->get();
     }
 }
