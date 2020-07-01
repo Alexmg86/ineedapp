83b848bcfa5b0b6be0874b5ebc183ed31d7aec6f
Alex <alexmg86@mail.ru>
2020-07-01 20:25:53 +0300

    Смена прав

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -3,85 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Access;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AccessController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return Access::all();
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        Access::updateOrCreate($request->all());
-        return User::with('acceses')->find($request->user_id);
-    }
+        $group_id = $request->group_id;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $can = \DB::table('group_user')->where([['group_id', $group_id], ['user_id', Auth::id()]])->count();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        if (!$can) {
+            return new Response(["У вас нет доступа"], 422);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        $data = $request->only(['group_id', 'role_id', 'user_id']);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if ($request->switch === 'true') {
+            Access::updateOrCreate($data);
+        } else {
+            Access::where($data)->delete();
+        }
+
+        // $userInfo = User::where('id', $request->user_id)->first();
+        // if ($userInfo) {
+        //     $is_owner = $userInfo->owner == $userInfo->id ? true : false;
+        //     $userInfo["roles"] = Role::when(!$is_owner, function ($query) use ($group_id, $userInfo) {
+        //         return $query->withCount(['acceses' => function ($query) use ($group_id, $userInfo) {
+        //             $query->where([['group_id', $group_id], ['user_id', $userInfo->id]]);
+        //         }]);
+        //     })->get()->map(function ($item, $key) use ($is_owner) {
+        //         $item->can = $is_owner || $item->acceses_count ? true : false;
+        //         return $item;
+        //     });
+        //     $userInfo->owner = $is_owner;
+        // }
+        return true;
     }
 }
